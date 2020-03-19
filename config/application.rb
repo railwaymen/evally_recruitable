f9e3f14741389328b01f9cdce067f795e37f4c64
Szymon Chodzidlo <schodevio@gmail.com>
2020-03-19 17:11:26 +0100

    Initialize testing tools

--- config/application.rb
+++ config/application.rb
@@ -26,6 +26,10 @@ module EvallyRecruitable
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.0
 
+    config.generators do |generate|
+      generate.factory_bot suffix: 'factory'
+    end
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -1,5 +1,22 @@
 # frozen_string_literal: true
 
+require 'simplecov'
+
+SimpleCov.start 'rails' do
+  add_filter '/spec/'
+  add_filter '/config/'
+  add_filter '/db/'
+  add_filter '/vendor/'
+
+  add_group 'Forms', 'app/forms'
+  add_group 'Services', 'app/services'
+  add_group 'Policies', 'app/policies'
+  add_group 'Presenters', 'app/presenters'
+  add_group 'Queries', 'app/queries'
+end
+
+SimpleCov.minimum_coverage 100
+
 # This file is copied to spec/ when you run 'rails generate rspec:install'
 require 'spec_helper'
 ENV['RAILS_ENV'] ||= 'test'
