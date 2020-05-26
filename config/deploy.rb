212a567829e40ecc86c9e858d592fec4aec7cf5b
Szymon Chodzidlo <schodevio@gmail.com>
2020-05-26 19:32:20 +0200

    Set linked dir for deploy

--- config/deploy.rb
+++ config/deploy.rb
@@ -27,7 +27,7 @@ set :repo_url, 'https://github.com/railwaymen/evally_recruitable.git'
 append :linked_files, 'config/database.yml', 'config/master.key', 'config/env.yml'
 
 # Default value for linked_dirs is []
-# append :linked_dirs, 'log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'public/system'
+append :linked_dirs, 'storage'
 
 # Default value for default_env is {}
 # set :default_env, { path: '/opt/ruby/bin:$PATH' }
