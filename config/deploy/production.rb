6e2f1e0e281a90e8b60093cab1bbf9b47665db39
Szymon Chodzidlo <schodevio@gmail.com>
2020-05-26 17:30:42 +0200

    Add sidekiq to deploy config

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -5,3 +5,11 @@ set :deploy_to, ENV['CAP_PRODUCTION_DEPLOY_TO']
 server ENV['CAP_PRODUCTION_SERVER'], user: ENV['CAP_PRODUCTION_SSH_USER'], roles: %w[app db web]
 set :rvm_ruby_version, 'ruby-2.7.0'
 set :branch, :master
+
+namespace :deploy do
+  after :publishing, :restart do
+    on roles :web do
+      execute 'systemctl --user restart sidekiq'
+    end
+  end
+end
--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -5,3 +5,11 @@ set :deploy_to, ENV['CAP_STAGING_DEPLOY_TO']
 server ENV['CAP_STAGING_SERVER'], user: ENV['CAP_STAGING_SSH_USER'], roles: %w[app db web]
 set :rvm_ruby_version, 'ruby-2.7.0'
 set :branch, :develop
+
+namespace :deploy do
+  after :publishing, :restart do
+    on roles :web do
+      execute 'systemctl --user restart sidekiq'
+    end
+  end
+end
