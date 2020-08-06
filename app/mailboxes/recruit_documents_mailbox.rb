fe718d91e10c388be5b28dd77176b995442e89d9
Szymon Chodzidlo <schodevio@gmail.com>
2020-08-06 10:55:49 +0200

    Update recruit document mailbox routing

--- app/mailboxes/recruit_documents_mailbox.rb
+++ app/mailboxes/recruit_documents_mailbox.rb
@@ -18,8 +18,21 @@ class RecruitDocumentsMailbox < ActionMailbox::Base
   private
 
   def source
-    recipient = mail.recipients.find { |r| MATCHER.match?(r) }
+    return senders_map[mail.from.first] if original_sender?
 
+    recipient = mail.recipients.find { |r| MATCHER.match?(r) }
     recipient[MATCHER, 1]
+  end
+
+  def original_sender?
+    senders_map.keys.include? mail.from.first
+  end
+
+  def senders_map
+    {
+      'info@railwaymen.org' => 'rwm',
+      'no-reply@justjoin.it' => 'justjoinit',
+      'no-reply@rocketjobs.pl' => 'rocketjobs'
+    }
   end
 end
--- spec/mailboxes/recruit_documents_mailbox_spec.rb
+++ spec/mailboxes/recruit_documents_mailbox_spec.rb
@@ -4,7 +4,7 @@ require 'rails_helper'
 
 RSpec.describe RecruitDocumentsMailbox, type: :mailbox do
   describe 'proper parsers calling' do
-    it 'expects to call rwm mail parser service' do
+    it 'expects to call rwm mail parser service base on suffix' do
       expect_any_instance_of(RecruitDocuments::RwmMailParserService).to receive(:perform)
 
       receive_inbound_email_from_mail(
@@ -13,7 +13,16 @@ RSpec.describe RecruitDocumentsMailbox, type: :mailbox do
       )
     end
 
-    it 'expects to call justjoinit mail parser service' do
+    it 'expects to call rwm mail parser service based on from attribute' do
+      expect_any_instance_of(RecruitDocuments::RwmMailParserService).to receive(:perform)
+
+      receive_inbound_email_from_mail(
+        from: 'info@railwaymen.org',
+        to: 'evallyrecruitable@example.com'
+      )
+    end
+
+    it 'expects to call justjoinit mail parser service based on suffix' do
       expect_any_instance_of(RecruitDocuments::JustjoinitMailParserService).to receive(:perform)
 
       receive_inbound_email_from_mail(
@@ -22,12 +31,30 @@ RSpec.describe RecruitDocumentsMailbox, type: :mailbox do
       )
     end
 
-    it 'expects to call rocketjobs mail parser service' do
+    it 'expects to call justjoinit mail parser service based on from attribute' do
+      expect_any_instance_of(RecruitDocuments::JustjoinitMailParserService).to receive(:perform)
+
+      receive_inbound_email_from_mail(
+        from: 'no-reply@justjoin.it',
+        to: 'evallyrecruitable@example.com'
+      )
+    end
+
+    it 'expects to call rocketjobs mail parser service base on suffix' do
       expect_any_instance_of(RecruitDocuments::RocketjobsMailParserService).to receive(:perform)
 
       receive_inbound_email_from_mail(
         from: 'jobs@example.com',
         to: 'evallyrecruitable+rocketjobs@example.com'
+      )
+    end
+
+    it 'expects to call rocketjobs mail parser service base on from attribute' do
+      expect_any_instance_of(RecruitDocuments::RocketjobsMailParserService).to receive(:perform)
+
+      receive_inbound_email_from_mail(
+        from: 'no-reply@rocketjobs.pl',
+        to: 'evallyrecruitable@example.com'
       )
     end
   end
