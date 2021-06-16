ddc937936bd2c810b92c76d0dd5c844963891a45
Szymon Chodzidlo <schodevio@gmail.com>
2021-06-16 22:11:05 +0200

    Fix classifier specs

--- spec/services/recruit_documents/group_classifier_service_spec.rb
+++ spec/services/recruit_documents/group_classifier_service_spec.rb
@@ -8,9 +8,12 @@ RSpec.describe RecruitDocuments::GroupClassifierService do
       FactoryBot.create(:recruit_document, group: 'Android', position: 'Junior Android Dev')
       FactoryBot.create(:recruit_document, group: 'iOS', position: 'iOS Dev')
 
-      classifier = instance_double ClassifierReborn::Bayes
+      classifier    = instance_double ClassifierReborn::Bayes
+      redis_backend = instance_double ClassifierReborn::BayesRedisBackend
 
       expect(ClassifierReborn::Bayes).to receive(:new).and_return(classifier)
+      expect(ClassifierReborn::BayesRedisBackend).to receive(:new).and_return(redis_backend)
+
       expect(classifier).to receive(:train).twice
 
       described_class.new.train
@@ -21,9 +24,12 @@ RSpec.describe RecruitDocuments::GroupClassifierService do
     it 'expects to call classify on classifier model' do
       document = FactoryBot.create(:recruit_document, group: 'Unknown', position: 'iOS Dev')
 
-      classifier = instance_double ClassifierReborn::Bayes
+      classifier    = instance_double ClassifierReborn::Bayes
+      redis_backend = instance_double ClassifierReborn::BayesRedisBackend
 
       expect(ClassifierReborn::Bayes).to receive(:new).and_return(classifier)
+      expect(ClassifierReborn::BayesRedisBackend).to receive(:new).and_return(redis_backend)
+
       expect(classifier).to receive(:classify).with(document.position)
 
       allow(Rails.env).to receive(:test?).and_return(false)
